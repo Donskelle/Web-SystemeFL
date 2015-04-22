612f71439f02f2678e0ea1627a7e684c360a699c
ZeroPit <peter.steensen.fl@googlemail.com>
2015-04-22 11:42:24 +0200

    Index.php Angepasst sodass sie die Aktuelle IP sich nimmt

--- html/index.php
+++ html/index.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Index Seite von DokuMummy</title>
+	<meta charset="UTF-8">
+	<title>Index Seite von DokuMummy</title>
 </head>
+	<body>
+		<h1>Wilkommen auf DokuMummy</h1>
+		<img src="index.png" alt="DokuMummy">
 
-
-<body>
-
-<h1>Wilkommen auf DokuMummy</h1>
-<img src="index.png" alt="DokuMummy">
-
-<p>
-  <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8001">CMS</a> DokuMummy<br>
-  <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8000">Framework</a> DokuMummy <a href="http://laravel.com/docs/5.0"> Laravel Doku</a> <br> 
-  <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8002">Test Seite</a> DokuMummy<br>
-</p>
-</body>
+		<p>
+		  Zur <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8000">Laravel Live Anischt</a> oder zur <a href="http://laravel.com/docs/5.0">Laravel Doku</a> <br> 
+		  Zur <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8001">Typo3 Neos Live Ansicht</a> oder zum <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8001/neos">Typo3 Neos Backend</a><br>
+		  Zur <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>:8002">Test Seite</a>
+		</p>
+	</body>
 </html>
